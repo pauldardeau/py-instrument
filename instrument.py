ba06d860ed383bdeb6261efc1e76e09e5025b762
Paul Dardeau <paul.dardeau@intel.com>
2016-06-02 09:42:04 -0500

    convert tabs to spaces

--- instrument.py
+++ instrument.py
@@ -11,50 +11,50 @@ def instrument(file_name):
     fn_name = ''
 
     for source_line in source_lines:
-    	stripped_source_line = source_line.strip()
+        stripped_source_line = source_line.strip()
 
-    	if len(stripped_source_line) > 0 and stripped_source_line[0] == '#':
-    		stripped_source_line = ''
+        if len(stripped_source_line) > 0 and stripped_source_line[0] == '#':
+            stripped_source_line = ''
 
-    	if len(stripped_source_line) == 0:
-    		instrumented += source_line
-    		instrumented += '\n'
-    	else:
-    		if instrument_line:
-    			num_whitespace_chars = len(source_line) - len(stripped_source_line)
-    			whitespace = source_line[0:num_whitespace_chars]
-    			injected_line = whitespace
-    			injected_line += "instrumentation.trace_fn('%s','%s','%s')" % (file_name, '', fn_name)
-    			instrumented += injected_line
-    			instrumented += '\n'
-    			instrument_line = False
-    			fn_name = ''
-    		pos_def = stripped_source_line.find('def ')
-    		if pos_def == 0:
-    			instrumented += source_line
-    			instrumented += '\n'
-    			pos_paren = stripped_source_line.find('(')
-    			if pos_paren > 0:
-    				fn_name = stripped_source_line[4:pos_paren]
-    				instrument_line = True
-    			else:
-    				print('error: unable to parse python source')
-    				sys.exit(1)
-    		else:
-    			instrumented += source_line
-    			instrumented += '\n'
+        if len(stripped_source_line) == 0:
+            instrumented += source_line
+            instrumented += '\n'
+        else:
+            if instrument_line:
+                num_whitespace_chars = len(source_line) - len(stripped_source_line)
+                whitespace = source_line[0:num_whitespace_chars]
+                injected_line = whitespace
+                injected_line += "instrumentation.trace_fn('%s','%s','%s')" % (file_name, '', fn_name)
+                instrumented += injected_line
+                instrumented += '\n'
+                instrument_line = False
+                fn_name = ''
+            pos_def = stripped_source_line.find('def ')
+            if pos_def == 0:
+                instrumented += source_line
+                instrumented += '\n'
+                pos_paren = stripped_source_line.find('(')
+                if pos_paren > 0:
+                    fn_name = stripped_source_line[4:pos_paren]
+                    instrument_line = True
+                else:
+                    print('error: unable to parse python source')
+                    sys.exit(1)
+            else:
+                instrumented += source_line
+                instrumented += '\n'
 
     return instrumented
 
 
 def main():
-	input_source_file = 'uninstrumented.py'
-	output_source_file = 'instrumented.py'
-	instrumented_code = instrument(input_source_file)
-	with open(output_source_file, 'w') as f:
-		f.write(instrumented_code)
-	print(instrumented_code)
+    input_source_file = 'uninstrumented.py'
+    output_source_file = 'instrumented.py'
+    instrumented_code = instrument(input_source_file)
+    with open(output_source_file, 'w') as f:
+        f.write(instrumented_code)
+    print(instrumented_code)
 
 
 if __name__=='__main__':
-	main()
+    main()
--- instrumentation.py
+++ instrumentation.py
@@ -1,24 +1,24 @@
 
 class Trace:
-	def __init__(self, file_name, class_name, method_name):
-		self.file_name = file_name
-		self.class_name = class_name
-		self.method_name = method_name
+    def __init__(self, file_name, class_name, method_name):
+        self.file_name = file_name
+        self.class_name = class_name
+        self.method_name = method_name
 
-	def to_string(self):
-		s = 'file=%s, class=%s, method=%s' % (self.file_name, self.class_name, self.method_name)
-		return s
+    def to_string(self):
+        s = 'file=%s, class=%s, method=%s' % (self.file_name, self.class_name, self.method_name)
+        return s
 
 
 def file_trace_fn(trace):
-	pass
+    pass
 
 
 def print_trace_fn(trace):
-	print(trace.to_string())
+    print(trace.to_string())
 
 
 def trace_fn(file_name, class_name, method_name):
-	trace = Trace(file_name, class_name, method_name)
-	print_trace_fn(trace)
-	file_trace_fn(trace)
+    trace = Trace(file_name, class_name, method_name)
+    print_trace_fn(trace)
+    file_trace_fn(trace)
